73175e110049ab9cce81ea058ab5eb8f54916075
Abdelrahman Tealab <abdelrahmantaelab@hotmail.com>
2024-02-17 22:37:07 -0500

    add fonts, create launch screen

---
+++ SuStash/SplashScreenView.swift
@@ -0,0 +1,45 @@
+//
+//  LaunchScreenView.swift
+//  SuStash
+//
+//  Created by Abdelrahman  Tealab on 2024-02-17.
+//
+
+import Foundation
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        if isActive {
+            // Proceed to your main content view
+            AuthenticationView()
+        } else {
+            VStack {
+                Image("sustashlogo") // Replace with your logo
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("SuStash")
+                    .font(.custom("Avenir Next", size: 48))
+                    .fontWeight(.thin)
+            }
+            .onAppear {
+                // Simulate a delay to mimic a real splash screen
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
--- SuStash/SuStashApp.swift
+++ SuStash/SuStashApp.swift
@@ -13,7 +13,7 @@ struct SuStashApp: App {
 
     var body: some Scene {
         WindowGroup {
-            AuthenticationView()
+            SplashScreenView()
         }
     }
 }
